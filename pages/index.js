b9c79ec529a53433922e0761f2ce64aa4c88733f
evsasse <evsasse@gmail.com>
2020-05-23 15:41:24 -0300

    testing dynamic react component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,39 @@
 import React from "react";
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
 function HomePage() {
   return (
     <main className="container mx-auto p-5 my-8">
       <h1>Can I fly?</h1>
+      <Clock />
     </main>
   );
 }
