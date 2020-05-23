477ba9da24ee0fb3ae3e3ea5e0f7cc9e897bac9e
José Ribeiro <jose@ae.studio>
2020-05-23 18:47:54 -0300

    Add made with love by 1313labs

--- components/index/Footer.js
+++ components/index/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => (
   <footer className="text-center">
-    <a href="https://github.com/1313labs/can-i-fly" className="text-3xl">
-      <i className="fab fa-fw mr-1 fa-github"></i>
+    <a href="https://github.com/1313labs/can-i-fly" className="text-3l">
+      Made with ❤️ by 1313labs <i className="fab fa-fw mr-1 fa-github"></i>
     </a>
   </footer>
 );
